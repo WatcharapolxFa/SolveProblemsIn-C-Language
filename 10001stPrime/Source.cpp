98dfc427b48e1b01593e2e03dcc330d589d32a53
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-07-01 23:12:44 +0700

    Source

--- 10001stPrime/Source.cpp
+++ 10001stPrime/Source.cpp
@@ -1,37 +1,38 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	int num[4]; // num[0] = answer  num[1] =number  num[2] =count  num[3] = flag
-	num[3] = num[2] = 0;
-	num[1] = num[0] = 1;
-	while (num[2]<1001)
+	int number[4]; // num[0] = answer  num[1] =number  num[2] =count  num[3] = flag
+	number[3] = number[2] = 0;
+	number[1] = number[0] = 1;
+	while (number[2] < 1001)
 	{
-		num[3] = 0; // current number is a prime number
-		if (num[1] == 1)
+		number[3] = 0; // current number is a prime number
+		if (number[1] == 1)
 		{
-			num[3] = 1;
+			number[3] = 1;
 		}
-		if (num[1] % 2 == 0)
+		if (number[1] % 2 == 0)
 		{
-			num[3] = 1;
+			number[3] = 1;
 		}
-		if (num[1] == 2)
+		if (number[1] == 2)
 		{
-			num[3] = 0;
+			number[3] = 0;
 		}
-		double boundary = (int)floor(sqrt(num[1]));
-		for (int f = 0; f <= boundary; f+=2)
+		double boundary = (int)floor(sqrt(number[1]));
+		for (int f = 0; f <= boundary; f += 2)
 		{
-			if (num[1] % f == 0)num[3] = 1;
-			if (num[3] == 0) 
+			if (number[1] % f == 0)
+				number[3] = 1;
+			if (number[3] == 0)
 			{
-				num[0] = num[1];
-				num[2]++;
-				printf_s("Count = %d , Prime =%d \n", num[2], num[0]);			
+				number[0] = number[1];
+				number[2]++;
+				printf_s("Count = %d , Prime =%d \n", number[2], number[0]);
 			}
-			num[1]++;
+			number[1]++;
 		}
 	}
 	return 0;
