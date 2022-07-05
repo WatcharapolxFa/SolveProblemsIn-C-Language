e63c9c37a32c114810bb969481361ade5640094f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-07-05 10:39:57 +0700

    Source

--- 0000A-BProblem/Source.cpp
+++ 0000A-BProblem/Source.cpp
@@ -1,24 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int positive(int, int);
 
 int main()
-{	
+{
 	struct ans
 	{
-		int num[2] , sum = 0;
-	}avg;
-	
-	for (int i = 0; i < 2; i++) {
+		int num[2], sum = 0;
+	} avg;
 
-		scanf_s("%d", &avg.num[i]);
+	for (int i = 0; i < 2; i++)
+	{
+		scanf("%d", &avg.num[i]);
 	}
 	avg.sum = positive(avg.num[0], avg.num[1]);
 	printf("%d", avg.sum);
-
 }
 
-int positive(int a , int b)
+int positive(int a, int b)
 {
 	int c = 0;
 	c = a + b;
