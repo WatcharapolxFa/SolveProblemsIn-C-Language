3378e5214d609f86abfd72712137791bb4913f8a
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-07-09 00:32:48 +0700

    Source

---
+++ LearnC/calculate.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Answer is : %d", 250 + 43);
+    return 0;
+}
---
+++ LearnC/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    /* code */
+    printf("Hello Language C");
+    return 0;
+}
