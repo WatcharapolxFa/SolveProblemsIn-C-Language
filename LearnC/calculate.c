7880cb2b08e9a289399bee7381f3df54515727fd
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-07-09 00:39:25 +0700

    Source

--- LearnC/calculate.c
+++ LearnC/calculate.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("Answer is : %d", 250 + 43);
+    printf("Answer is plus : 250 + 43 = %d\n", 250 + 43);
+    printf("Answer is minus: 250 - 43 = %d", 250 - 50);
     return 0;
 }
