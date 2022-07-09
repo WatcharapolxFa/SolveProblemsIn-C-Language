46cac422fadd17ee3360ce3ca8ac3ef7de54492e
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-07-09 13:36:38 +0700

    Succeed Variable

---
+++ LearnC/Variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int bananaPrice = 8;
+    int unit = 10;
+
+    int totalPrice = bananaPrice * unit;
+    printf("Total price  = %d B.", totalPrice);
+
+    return 0;
+}
