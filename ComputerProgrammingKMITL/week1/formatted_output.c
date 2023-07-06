608dd6072a05819449f5ec1a01db07dd53d90464
watcharapol Yotadee <watcharapolyotadee@watcharapols-MacBook-Pro.local>
2023-07-06 20:50:25 +0700

    feat:update file learn in kmitl

---
+++ ComputerProgrammingKMITL/week1/formatted_output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("%-80s\n", "Hello, world!");
+    printf("%-80s\n", "Hello, Ladkrabang");
+    printf("%-80s\n", "Hello, Computer Programming");
+    printf("%-8s%8s%8s%8s%8s%12s\n", "one", "two", "three", "four", "five", "Hallelujah");
+    printf("%-8s%8s%8s%8s%8s\n", "12345678", "12345678", "12345678", "12345678", "12345678");
+    printf("%8s%8s%8s%8s%8s\n", "12345678", "12345678", "12345678", "12345678", "12345678");
+
+    return 0;
+}
---
+++ ComputerProgrammingKMITL/week2/display_message.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+    printf("%-70s%10s\n", "Programming", "Computer");
+    return 0;
+}
